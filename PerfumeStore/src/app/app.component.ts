fe779d44c99e5b52d75255c4affb053e9a394d60
zrsoo <46485193+zrsoo@users.noreply.github.com>
2022-10-15 16:26:34 +0300

    Setup

---
+++ PerfumeStore/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:`
+    
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'PerfumeStore';
+}
